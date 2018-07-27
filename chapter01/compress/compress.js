a7023e7c111b22e58879a74180e3e39c2fa4c326
ray <rmartone@gmail.com>
2018-07-27 13:12:31 -0400

    cleaning up

--- chapter01/compress/compress.js
+++ chapter01/compress/compress.js
@@ -1,15 +1,17 @@
 export function compress(str) {
   const compressed = [];
-  let count = 0;
+  let currentCount = 0;
   for (let i = 0; i < str.length; i += 1) {
-    ++count;
+    ++currentCount;
     let currentChar = str[i];
-    const nextChar = i + 1;
-    if (nextChar === str.length || currentChar !== str[nextChar]) {
+    const nextIndex = i + 1;
+    // are we at a new char or the end of string?
+    if (nextIndex === str.length || currentChar !== str[nextIndex]) {
       compressed.push(currentChar);
-      compressed.push(count);
-      count = 0;
+      compressed.push(currentCount);
+      currentCount = 0;
     }
   }
+  // if the original string is shorter than the compressed return it
   return compressed.length < str.length ? compressed.join('') : str;
 }
